5706f923903836a4f1fcb9f7e56f7099e783cb55
C4Q <c4q@C4Qs-MBP-42.home>
2016-09-20 03:51:13 -0400

    updated

--- src/nyc/c4q/ramonaharrison/channelHop.java
+++ src/nyc/c4q/ramonaharrison/channelHop.java
@@ -11,7 +11,12 @@ import static nyc.c4q.ramonaharrison.network.Slack.delay;
  * Created by c4q on 9/20/16.
  */
 
-
+/** Rafat:  I was thinking that I can create a loop like a game, to have the bot migrate between channels and create
+ *          A standard of difficulty and a creative element in chat. The bot will engage in asking users questions about
+ *          each other. IT will prompt another user to answer a question of a user in the chat. The user will receive penalty
+ *          for wrong answers and will damage the bot for right answers. The bot will go through sleep mode and upgrade
+ *          to different "Alert Levels"
+ **/
 public class channelHop {
 
     private static String general = "C2CSB9HD2";
